683c1ad13b3c1f5bafde22b5de4ad558810baa5c
Denis Pitul <denis.pitul@gmail.com>
2013-12-19 22:45:01 +0200

    adding models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Share(models.Model):
+
+class Friend(models.Model):
+
+class User(models.Model):
--- login/models.py
+++ login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class SecurityQuestion(models.Model):
+    question_text = models.CharField(max_length = 128)
+
+class User(models.Model):
+    name = models.CharField(max_length = 50)
+    password = models.CharField(max_length = 128)
+    sec_question = models.ForeignKey(SecurityQuestion)
+    sec_question_answer = models.CharField(max_length = 128)
